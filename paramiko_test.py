5eea8f8c1189efbd6bcd5cd81629bff59ed33f04
Greg Cotten <gregcotten@gmail.com>
2012-09-06 19:40:43 -0700

    removed the FTP test I accidentally left in the folder.

--- paramiko_test.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-host = "gregcotten.com"                    #hard-coded
-port = 22
-transport = paramiko.Transport((host, port))
-
-transport.connect(username = "gregcott", password = "fakepass")
-
-ftp = paramiko.SFTPClient.from_transport(transport)
